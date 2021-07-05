a69b9b0fdc90d3203ee7fd1b121f222fb1ca0579
Igor Silva <osilvaigor@gmail.com>
2021-07-05 00:04:53 -0300

    Resolução do desafio 2: Trabalhando com middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,84 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    response.status(404).json({
+      error: "User not found!"
+    })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.todos.length < 10 || user.pro) {
+    return next();
+  }
+
+  return response.status(403).json({
+    error: "You can't create a new To-Do!"
+  })
+
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  const validUuid = validate(id);
+  
+  if (!user) {
+    return response.status(404).json({
+      error: "User not found!"
+    })
+  }
+  
+  
+  if (!validUuid) {
+    return response.status(400).json({
+      error: "It is not a valid uuid!"
+    })
+  }
+  
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+  
+  if (todoIndex === -1) {
+    return response.status(404).json({
+      error: "ID not found!"
+    })
+  }
+  
+  request.user = user;
+  request.todo = user.todos[todoIndex];
+  
+  return next();
+  
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  
+  const userIndex = users.findIndex((user) => user.id === id);
+  
+  if (userIndex === -1) {
+    return response.status(404).json({
+      error: "User not found!"
+    })
+  }
+
+  request.user = users[userIndex];
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
